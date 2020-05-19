e3b796fab313bcc2b2ed3880ed36603f6a6c0fe2
shubham14p3 <shubham14p3@gmail.com>
2020-05-20 02:27:46 +0530

    Restarting the Project

--- app/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user, foreign_key: "user_id"
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'sessions/sessions' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :posts, :only =>[:new, :create, :index]
-  root to: "posts#index"
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
-
-end
