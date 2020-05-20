e9983b3cef9ab224f777c38163ae13d6adbc07f9
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-20 10:31:10 -0500

    Fixed the posts being showed by changing the root directory to always point to the Post controller. Added the logic to show and hide depending on whether the user is signed in or not. Completed project requirements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 	  @post.user = current_user
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "posts#index"
 
 end
