d7f5b3d0ef05f18eac964bcb14e03db7b9ef080b
shubham14p3 <shubham14p3@gmail.com>
2020-05-20 20:41:11 +0530

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   #devise_for :users
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
